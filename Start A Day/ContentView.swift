e234a97482b291b54bc3b3bd00f7cec587b17887
bellyu <bellyu@easontech.com.tw>
2021-07-01 22:48:27 +0800

    Initial Commit

---
+++ Start A Day/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Start A Day
+//
+//  Created by 游輝懋 on 2021/7/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
